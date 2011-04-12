29ae94c5f0503d3045c55b233680c96fd8bd3070
dveyarangi <chukchac@yahoo.com>
2011-04-13 00:36:43 +0300

    egit bug test

--- quadraturin-graphics/src/main/java/yarangi/graphics/utils/scene/BackgroundLook.java
+++ quadraturin-graphics/src/main/java/yarangi/graphics/utils/scene/BackgroundLook.java
@@ -4,7 +4,13 @@ import javax.media.opengl.GL;
 
 import yarangi.graphics.quadraturin.RenderingContext;
 import yarangi.graphics.quadraturin.objects.Look;
+import yarangi.numbers.RandomUtil;
 
+/**
+ * TODO: add texture and bump map 
+ * @author dveyarangi
+ *
+ */
 public class BackgroundLook implements Look<BackgroundEntity>
 {
 
@@ -24,28 +30,33 @@ public class BackgroundLook implements Look<BackgroundEntity>
 		double halfHeight = be.getHeight()/2;
 		double tileSize = be.getTileSize();
 		
-		gl.glLightfv(GL.GL_LIGHT1, GL.GL_AMBIENT, this.lightAmbient, 0);
-      gl.glLightfv(GL.GL_LIGHT1, GL.GL_DIFFUSE, this.lightDiffuse, 0);
-      gl.glLightfv(GL.GL_LIGHT1, GL.GL_POSITION, be.getLightPosition(), 0);
-     gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPOT_DIRECTION, lightDirection, 0);    
-      gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPOT_CUTOFF, lightCutOff, 0);    
-      gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPOT_EXPONENT, lightExponent, 0);    
+//		gl.glLightfv(GL.GL_LIGHT1, GL.GL_AMBIENT, this.lightAmbient, 0);
+//      gl.glLightfv(GL.GL_LIGHT1, GL.GL_DIFFUSE, this.lightDiffuse, 0);
+//      gl.glLightfv(GL.GL_LIGHT1, GL.GL_POSITION, be.getLightPosition(), 0);
+//     gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPOT_DIRECTION, lightDirection, 0);    
+ //     gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPOT_CUTOFF, lightCutOff, 0);    
+ //     gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPOT_EXPONENT, lightExponent, 0);    
      
-      gl.glEnable(GL.GL_LIGHT1);
-      gl.glEnable(GL.GL_LIGHTING);
+ //     gl.glEnable(GL.GL_LIGHT1);
+ //     gl.glEnable(GL.GL_LIGHTING);
       if(meshListId == -1)
       {
     	  meshListId=gl.glGenLists(1);
     	  gl.glNewList(meshListId,GL.GL_COMPILE);	
-			gl.glColor4f(0.0f, 0.0f, 0.0f, 1.0f);
+//			gl.glColor4f((float)RandomUtil.getRandomDouble(0.5), (float)RandomUtil.getRandomDouble(0.5), (float)RandomUtil.getRandomDouble(0.5), 1.0f);
 			gl.glBegin(GL.GL_QUADS);
-			
+		
 			for(int x = 0; x < heights[0].length-1; x ++)
 			{
 				double realx = tileSize*x;
 	
 				for(int y = 0; y < heights.length-1; y ++)
 				{
+					if((x % 2) + (y % 2) == 1)
+						gl.glColor4f(0.0f, 0.0f, 0.0f, 1.0f);
+					else
+						gl.glColor4f(0.5f, 0.5f, 0.5f, 1.0f);
+					
 					double realy = tileSize * y;
 					gl.glVertex3f((float)(realx-halfWidth), (float)(realy-halfHeight), (float)heights[x][y]);
 					gl.glVertex3f((float)(realx-halfWidth+tileSize), (float)(realy-halfHeight), (float)heights[x+1][y]);
@@ -62,8 +73,8 @@ public class BackgroundLook implements Look<BackgroundEntity>
  //   lightDirection[0] += 0.0001;
 //       be.getLightPosition()[2] += 0.0001;
  //     lightExponent[0] -= 0.1;
-      gl.glDisable(GL.GL_LIGHT1);
-      gl.glDisable(GL.GL_LIGHTING);
+//      gl.glDisable(GL.GL_LIGHT1);
+//      gl.glDisable(GL.GL_LIGHTING);
 
 	}
 
