85988bc7b6493a0d95dd1e0a472026a0b16f336d
dveyarangi <chukchac@yahoo.com>
2011-06-01 01:45:32 +0300

    You go there too.

---
+++ quadraturin-utils/src/main/java/yarangi/spatial/IAreaChunk.java
@@ -0,0 +1,13 @@
+package yarangi.spatial;
+
+public interface IAreaChunk 
+{
+	public Area getArea();
+	public double getX();
+	public double getY();
+	public boolean overlaps(double xmin, double ymin, double xmax, double ymax);
+	public double getMinX();
+	public double getMinY();
+	public double getMaxX();
+	public double getMaxY();
+}
