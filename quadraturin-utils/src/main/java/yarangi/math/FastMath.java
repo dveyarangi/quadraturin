24b964d875af6ea7caa5ad6d952196efb77df166
dveyarangi <chukchac@yahoo.com>
2011-06-02 19:10:56 +0300

    Some math functions.

---
+++ quadraturin-utils/src/main/java/yarangi/math/FastMath.java
@@ -0,0 +1,29 @@
+package yarangi.math;
+
+public class FastMath 
+{
+	   private static final int    BIG_ENOUGH_INT   = 16 * 1024;
+	   private static final double BIG_ENOUGH_FLOOR = BIG_ENOUGH_INT;
+	   private static final double BIG_ENOUGH_ROUND = BIG_ENOUGH_INT + 0.5;
+	 
+	   public static int floor(double x) {
+	      return (int) (x + BIG_ENOUGH_FLOOR) - BIG_ENOUGH_INT;
+	   }
+	 
+	   public static int round(double x) {
+	      return (int) (x + BIG_ENOUGH_ROUND) - BIG_ENOUGH_INT;
+	   }
+	 
+	   public static int ceil(double x) {
+	       return BIG_ENOUGH_INT - (int)(BIG_ENOUGH_FLOOR-x); // credit: roquen
+	   }
+	   
+	   public static int toGrid(int val, int cell)
+	   {
+		   return val/cell*cell; // embracing eternity!
+	   }
+	   public static double toGrid(double val, double cell)
+	   {
+		   return round(val/cell)*cell;
+	   }
+}
