436a0adff513d6f618b8f9e46364cd12f910108f
dveyarangi <chukchac@yahoo.com>
2011-07-23 14:11:35 +0300

    Adapted to SceneEntity composition scheme.

--- quadraturin-core/src/main/java/yarangi/graphics/quadraturin/objects/Dummy.java
+++ quadraturin-core/src/main/java/yarangi/graphics/quadraturin/objects/Dummy.java
@@ -4,12 +4,11 @@ import javax.media.opengl.GL;
 
 import yarangi.graphics.quadraturin.RenderingContext;
 import yarangi.graphics.quadraturin.objects.behaviors.DummyBehavior;
-import yarangi.spatial.Area;
 
 /**
  * Dummy entity for torturing needs. 
  */
-public class Dummy extends CompositeSceneEntity
+public class Dummy extends SceneEntity
 {
 
 	private static final long serialVersionUID = 5263046347119077749L;
@@ -17,22 +16,11 @@ public class Dummy extends CompositeSceneEntity
 	public static Look <SceneEntity> LOOK = new Look <SceneEntity> () { 
 		public void render(GL gl, double time, SceneEntity entity, RenderingContext context) {}
 		public void init(GL gl, SceneEntity entity) { } 
-		public void destroy(GL gl, SceneEntity entity) { } 
+		public void destroy(GL gl, SceneEntity entity) { }
+		@Override
+		public boolean isCastsShadow() { return false; } 
 	};
 	
 	public static Behavior <SceneEntity> BEHAVIOR = new DummyBehavior <SceneEntity> ();
-	public Dummy() 
-	{ 
-		setLook(LOOK);
-		setBehavior(BEHAVIOR);
-		setArea(Area.EMPTY);
-	}
 
-	@Override
-	public boolean isPickable() {
-		return false;
-	}
-
-
-	
 }
