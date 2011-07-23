c9719d50180d6321a70d9e645666ac910cb714a9
dveyarangi <chukchac@yahoo.com>
2011-07-23 14:09:59 +0300

    After long series of transformations, The DefaultActionFactory seems to be redundant once again.

--- quadraturin-core/src/main/java/yarangi/graphics/quadraturin/actions/DefaultActionFactory.java
+++ quadraturin-core/src/main/java/yarangi/graphics/quadraturin/actions/DefaultActionFactory.java
@@ -3,17 +3,18 @@ package yarangi.graphics.quadraturin.actions;
 import java.util.Map;
 
 import yarangi.graphics.quadraturin.IViewPoint;
+import yarangi.graphics.quadraturin.Scene;
 import yarangi.graphics.quadraturin.ViewPoint2D;
 import yarangi.graphics.quadraturin.config.InputConfig;
-import yarangi.graphics.quadraturin.config.XMLQuadConfig;
+import yarangi.graphics.quadraturin.config.QuadConfigFactory;
 import yarangi.graphics.quadraturin.events.UserActionEvent;
 
 public class DefaultActionFactory 
 {
-	public static Map <String, IAction> fillNavigationActions(Map <String, IAction> actions, IViewPoint viewPoint)
+	public static Map <String, IAction> fillNavigationActions(final Scene scene, Map <String, IAction> actions, IViewPoint viewPoint)
 	{
 		final ViewPoint2D vp = (ViewPoint2D) viewPoint;
-		InputConfig config = XMLQuadConfig.getInstance().getInputConfig();
+		InputConfig config = QuadConfigFactory.getConfig().getInputConfig();
 		
 		final double scrollStep = config.getScrollStep();
 		final double scaleStep = config.getScaleStep();
@@ -21,7 +22,7 @@ public class DefaultActionFactory
 		
 		
 		actions.put("scroll-right", new IAction() {
-				public void act(UserActionEvent event) {vp.getCenter().x -= scrollStep/vp.getScale(); }}
+			public void act(UserActionEvent event) { vp.getCenter().x -= scrollStep/vp.getScale(); }}
 			);
 		actions.put("scroll-left", new IAction() {
 			public void act(UserActionEvent event) { vp.getCenter().x += scrollStep/vp.getScale(); }}
@@ -36,12 +37,14 @@ public class DefaultActionFactory
 			public void act(UserActionEvent event) {
 				double s = vp.getScale() * scaleStep;
 				vp.setScale(s > vp.getMinScale() ? s : vp.getMinScale()); 
+				scene.setFrameLength(vp.getScale()*2);
 			}}
 		);
 		actions.put("zoom-out", new IAction() {
 			public void act(UserActionEvent event) {
 				double s = vp.getScale() / scaleStep;
 				vp.setScale(s < vp.getMaxScale() ? s : vp.getMaxScale());
+				scene.setFrameLength(vp.getScale()*2);
 			 }}
 		);
 
