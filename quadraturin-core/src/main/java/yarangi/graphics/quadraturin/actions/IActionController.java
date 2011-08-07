6228a65ebaefda1c6f47b09c7d954cc7cad838ec
dveyarangi <chukchac@yahoo.com>
2011-08-07 15:32:11 +0300

    Introduced IActionController to make action binding more flexible.

---
+++ quadraturin-core/src/main/java/yarangi/graphics/quadraturin/actions/IActionController.java
@@ -0,0 +1,22 @@
+package yarangi.graphics.quadraturin.actions;
+
+import java.util.Set;
+
+import javax.media.opengl.GL;
+
+import yarangi.graphics.quadraturin.RenderingContext;
+import yarangi.graphics.quadraturin.events.CursorListener;
+import yarangi.graphics.quadraturin.events.UserActionListener;
+import yarangi.graphics.quadraturin.objects.Look;
+import yarangi.graphics.quadraturin.objects.SceneEntity;
+import yarangi.spatial.ISpatialFilter;
+
+
+public interface IActionController extends UserActionListener, CursorListener
+{
+	public abstract Set <String> getActionIds();
+	public ISpatialFilter <SceneEntity> getPickingFilter();
+	
+	public Look <? extends IActionController> getLook();
+	public abstract void display(GL gl, double time, RenderingContext context);
+}
