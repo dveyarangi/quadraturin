b84337376b1d2246d8f9049e0dc8fc58c85566bd
dveyarangi <chukchac@yahoo.com>
2011-08-07 15:27:23 +0300

    Added single selection query processor class.

---
+++ quadraturin-utils/src/main/java/yarangi/spatial/PickingSensor.java
@@ -0,0 +1,29 @@
+package yarangi.spatial;
+
+
+public class PickingSensor <K> implements ISpatialSensor <K> 
+{
+
+	private static final long serialVersionUID = 9025712177585233445L;
+	
+	private ISpatialFilter <K> filter;
+	
+	private K object;
+	
+	public PickingSensor(ISpatialFilter <K> filter)
+	{
+		this.filter = filter;
+	}
+
+	public boolean objectFound(IAreaChunk chunk, K object) {
+		if(filter.accept( object ))
+		{
+			this.object = object;
+			return true; // terminating query
+		}
+		return false;
+	}
+	
+	public K getObject() { return object; }
+
+}
