5f1f03d1168af48de4493cf22938c75f756d8efc
dveyarangi <chukchac@yahoo.com>
2011-08-21 12:11:45 +0300

    Added unsupported method reporting function

--- quadraturin-utils/src/main/java/yarangi/ZenUtils.java
+++ quadraturin-utils/src/main/java/yarangi/ZenUtils.java
@@ -8,7 +8,12 @@ public class ZenUtils
 		boolean right = false;
 		
 		if(left == right || left == right) 
-			throw new IllegalStateException("Software refuses to start in context of unstable reatilty.");
+			throw new IllegalStateException("Program refuses to start in context of unstable reatilty.");
 
+	}
+	
+	public static void methodNotSupported(Class claass)
+	{
+		throw new IllegalStateException("This method is not supported for class " + claass);
 	}
 }
