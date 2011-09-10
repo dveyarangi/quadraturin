5cbaac06a04932193a3900a927773c21bde96c66
dveyarangi <chukchac@yahoo.com>
2011-09-10 12:36:15 +0300

    added configuration logger

--- quadraturin-core/src/main/java/yarangi/graphics/quadraturin/config/IQuadConfig.java
+++ quadraturin-core/src/main/java/yarangi/graphics/quadraturin/config/IQuadConfig.java
@@ -1,8 +1,11 @@
 package yarangi.graphics.quadraturin.config;
 
+import org.apache.log4j.Logger;
+
 public interface IQuadConfig 
 {
-
+	public static final Logger LOG = Logger.getLogger( "q-config" );
+	
 	public EkranConfig getEkranConfig();
 
 	public StageConfig getStageConfig();
