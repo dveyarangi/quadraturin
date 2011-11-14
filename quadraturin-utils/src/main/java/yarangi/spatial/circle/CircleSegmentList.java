3b764eec2371f15bc9a2688826681c11df0a8e93
dveyarangi <chukchac@yahoo.com>
2011-11-14 20:12:59 +0200

    i'm just can't do it

--- quadraturin-utils/src/main/java/yarangi/spatial/circle/CircleSegmentList.java
+++ quadraturin-utils/src/main/java/yarangi/spatial/circle/CircleSegmentList.java
@@ -11,7 +11,7 @@ public class CircleSegmentList
 	/**
 	 * Special holder for interval around pi/2 (-pi/2)
 	 */
-	private AngleInterval mergingInterval;
+	private AngleInterval closureInterval;
 	
 	public CircleSegmentList()
 	{
@@ -23,6 +23,12 @@ public class CircleSegmentList
 	 */
 	public void add(AngleInterval inew)
 	{
+		boolean inverse = inew.getMin() > inew.getMax();
+		if(inverse && closureInterval == null)
+		{
+			closureInterval = inew;
+		}
+		else
 		if(intervals.isEmpty())
 		{
 			intervals.add(inew);
@@ -31,44 +37,92 @@ public class CircleSegmentList
 		ListIterator <AngleInterval> it = intervals.listIterator();
 		AngleInterval imin = null;
 		AngleInterval imax = null;
+		AngleInterval modifiedInterval;
 
 		boolean minFound = false;
 		boolean maxFound = false;
+		boolean removeInterval = true;
+		
 		AngleInterval inext = null;
+		
 		while(it.hasNext())
 		{
-			if(inext != null) {
-				if(inew.getMin() > inew.getMax())
-				{
-					if(minFound || !maxFound)
-						it.remove();
-				}
-				else
-				{
-					if(minFound && !maxFound)
-						it.remove();
-				}
-			}
-			
 			inext = it.next();
-			if(inext.getMax() <= inew.getMin()) {
-				imin = inext.getMax() < inew.getMin() ? null : inext;
-				minFound = true;
+
+			if(inverse)
+			{
+
+				
+				if(inext.getMax() >= inew.getMin()) {
+					minFound = true;
+					if(inext.getMin() < inew.getMin())
+						imin = inext;
+				}
+				boolean replacementZone = (minFound || !maxFound) ;
+				
+				
+				if(inext.getMax() >= inew.getMax() && !maxFound) {
+					maxFound = true;
+					if(inext.getMin() > inew.getMax())
+						removeInterval = false;
+					else
+						imax = inext;
+				}		
+				
+				
+				if(replacementZone && removeInterval) {
+					it.remove();
+				}
+				removeInterval = true;
+				
 			}
-			if(inext.getMax() >= inew.getMax()) {
-				imin = inext.getMin() > inew.getMax() ? null : inext;
+			else
+			{
+				if(inext.getMax() >= inew.getMin() && !minFound) {
+					minFound = true;
+					if(inext.getMin() > inew.getMax())
+						removeInterval = false; // we skipped over this
+					else
+						imin = inext;
+				}
+				
+				boolean replacementZone = minFound && !maxFound ;
+				
+				if(inext.getMax() >= inew.getMax()) {
+					maxFound = true;
+					if(inext.getMin() > inew.getMax() && !minFound)
+						removeInterval = false; // we skipped over this
+					else
+						imax = inext;
+				}
+				
+				if(replacementZone && removeInterval) {
+					it.remove();
+				}
+				removeInterval = true;
+
 			}
+
+			
 		}
+		boolean mergeWithIntverse = closureInterval != null && 
+				(inew.getMin() <= closureInterval.getMax() ||
+				 inew.getMax() >= closureInterval.getMin());
+		if(inverse || mergeWithIntverse)
+			closureInterval = new AngleInterval(imin == null ? Math.max(inew.getMin(), closureInterval.getMin()) : imin.getMin(), 
+											    imax == null ? Math.min(inew.getMax(), closureInterval.getMax()) : imax.getMax());
+		else
+			it.add(new AngleInterval(imin == null ? inew.getMin() : imin.getMin(),
+									 imax == null ? inew.getMax() : imax.getMax()));
 	}
 	
 	public static void main(String ... args) 
 	{
 		CircleSegmentList list = new CircleSegmentList();
 		
-		list.add(new AngleInterval(1.5, 2));
-		list.add(new AngleInterval(2, 3));
-		list.add(new AngleInterval(4, -3.5));
-		list.add(new AngleInterval(5, -.15));
+		list.add(new AngleInterval(1, 2));
+		list.add(new AngleInterval(2.5, 3));
+		list.add(new AngleInterval(2.4, -0.5));
 		
 		for(AngleInterval i : list.intervals)
 			System.out.println(i);
