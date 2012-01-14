b88779b7c4212ce019b81572e08e77b9f486b4b5
dveyarangi <chukchac@yahoo.com>
2012-01-14 18:30:44 +0200

    Fixed frame buffer bugs; debug prettified

--- quadraturin-graphics/src/main/java/yarangi/graphics/grid/TileGridLook.java
+++ quadraturin-graphics/src/main/java/yarangi/graphics/grid/TileGridLook.java
@@ -11,20 +11,31 @@ import yarangi.graphics.quadraturin.Q;
 import yarangi.graphics.quadraturin.objects.Look;
 import yarangi.graphics.textures.FBO;
 import yarangi.graphics.textures.TextureUtils;
-import yarangi.math.BitUtils;
 import yarangi.spatial.IGrid;
 import yarangi.spatial.IGridListener;
 import yarangi.spatial.Tile;
 
+/**
+ * This is a generic renderer for bigger grids
+ * Renders into a dedicated frame buffer.
+ * Inheriting classes must define {@link Tile} rendering procedure.
+ * Only tiles that where explicitly marked "modified" by grid implementation
+ * will be redrawn at rendering turn.
+ * 
+ * @author dveyarangi
+ *
+ * @param <O>
+ * @param <G>
+ */
 public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Look <G>, IGridListener<Tile<O>>
 {
 	private FBO fbo;
 	
-	private int gridTextureWidth, gridTextureHeight;
-	
 	private Collection <Tile<O>> pendingTiles;
 	
-	private boolean debug = Boolean.valueOf( System.getProperty( "yarangi.graphics.quadraturin.debug.tilegrid" ));
+	private double gridWidth, gridHeight;
+	
+	private boolean debug = true;
 	
 	private Point dimensions;
 	
@@ -35,16 +46,21 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 	{
 		Q.rendering.debug( "Initializing tiled grid renderer for [" + grid + "]...");
 		// rounding texture size to power of 2:
-		Point dim = getFBODimensions( grid );
-		this.dimensions = new Point(BitUtils.po2Ceiling(  (int)(grid.getMaxX()-grid.getMinX()) ), BitUtils.po2Ceiling( (int)(grid.getMaxY()-grid.getMinY()) ));
+ 		this.dimensions = getFBODimensions( grid );
+//		this.dimensions = new Point(BitUtils.po2Ceiling(  (int)(grid.getMaxX()-grid.getMinX()) ), BitUtils.po2Ceiling( (int)(grid.getMaxY()-grid.getMinY()) ));
+//		this.dimensions = new Point( 512, 512);
 		// TODO: divide to 1024x1024 textures
 		int texture = TextureUtils.createEmptyTexture2D( gl, dimensions.x, dimensions.y, false );
 		Q.rendering.debug( "Created terrain FBO texture - size:[" + dimensions.x + "x" + dimensions.y + "], texture id:" + texture);
 		
 		gl.glBindTexture( GL.GL_TEXTURE_2D, texture );
+//		gl.glTexEnvf(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE, GL.GL_REPLACE);
 		gl.glTexParameteri( GL.GL_TEXTURE_2D,  GL.GL_TEXTURE_MIN_FILTER,  GL.GL_NEAREST);
 		gl.glTexParameteri( GL.GL_TEXTURE_2D,  GL.GL_TEXTURE_MAG_FILTER,  GL.GL_NEAREST);		
 		gl.glBindTexture( GL.GL_TEXTURE_2D, 0 );
+		
+		gridWidth = grid.getMaxX() - grid.getMinX();
+		gridHeight = grid.getMaxY() - grid.getMinY();
 		
 		fbo = FBO.createFBO( gl, texture, TextureUtils.ILLEGAL_ID );
 		Q.rendering.debug( "Created terrain FBO - id:" + fbo.getFboId());
@@ -53,12 +69,15 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 		
 		updateFrameBuffer( gl, grid );
 		
-		if(debug)
-			initDebug(gl, grid);
+		assert initDebug(gl, grid);
 	}
 	
 
-
+	/** 
+	 * Defines dimensions terrain render buffer (should be rounded to n^2) 
+	 * @param grid
+	 * @return
+	 */
 	protected abstract Point getFBODimensions(G grid);
 
 	@Override
@@ -66,7 +85,7 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 	{
 		// redrawing changed tiles to frame buffer
 		updateFrameBuffer( gl, grid );
-
+// 
 		float minx = grid.getMinX();
 		float maxx = grid.getMaxX();
 		float miny = grid.getMinY();
@@ -74,8 +93,10 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 		
 		// rendering frame buffer texture:
 //		fbo.unbind( gl );
+		gl.glDisable( GL.GL_DEPTH_TEST );
 		fbo.bindTexture(gl);
 			gl.glBegin(GL.GL_QUADS);
+			gl.glColor4f( 0, 1, 0, 1 );
 				gl.glTexCoord2f(0,0); gl.glVertex2f( minx, miny );
 				gl.glTexCoord2f(0,1); gl.glVertex2f( minx, maxy );
 				gl.glTexCoord2f(1,1); gl.glVertex2f( maxx, maxy );
@@ -83,15 +104,16 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 			gl.glEnd();
 		fbo.unbindTexture(gl);
 		
-		if(debugMesh != null)
-			debugMesh.call( gl );
+//		gl.glEnable(GL.GL_BLEND);
+		
+		assert renderDebug(gl);
 		
 	}
+
 
 	@Override
 	public void destroy(GL gl, G grid, IRenderingContext context)
 	{
-		
 		grid.setModificationListener( null );
 		
 		fbo.destroy( gl );
@@ -113,21 +135,21 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 		pendingTiles = tiles;
 	}
 
-	private void beginFrameBufferSpace(GL gl, G grid)
+	protected void beginFrameBufferSpace(GL gl, G grid)
 	{
 		// transforming the rendering plane to fit the terrain grid:
 		gl.glPushAttrib(GL.GL_VIEWPORT_BIT | GL.GL_ENABLE_BIT);	
-		gl.glDisable(GL.GL_BLEND);
+//		gl.glDisable(GL.GL_BLEND);
 		gl.glMatrixMode(GL.GL_MODELVIEW); gl.glPushMatrix();  gl.glLoadIdentity();
 		gl.glMatrixMode(GL.GL_PROJECTION); gl.glPushMatrix(); gl.glLoadIdentity();
-		gl.glViewport(0,0,gridTextureWidth, gridTextureHeight);
-		gl.glOrtho(-dimensions.x/2, dimensions.x, -dimensions.y/2, dimensions.y, -1, 1);
+		gl.glViewport(0,0,dimensions.x, dimensions.y);
+		gl.glOrtho(grid.getMinX(), grid.getMaxX(), grid.getMinY(), grid.getMaxY(), -1, 1);
 
 		fbo.bind(gl);
 		gl.glEnable( GL.GL_BLEND );
 	}
 	
-	private void endFrameBufferSpace(GL gl)
+	protected void endFrameBufferSpace(GL gl)
 	{
 		fbo.unbind(gl);
 		
@@ -136,29 +158,44 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 		gl.glPopAttrib();	
 	}
 	
+	/**
+	 * 
+	 * @param gl
+	 * @param grid
+	 */
 	private void updateFrameBuffer(GL gl, G grid)
 	{
 		if(pendingTiles != null && pendingTiles.size() > 0)
 		{
-			// redrawing changed tiles:
-			
+
 			beginFrameBufferSpace( gl, grid );
+			// TODO: listen to viewpont changes and draw only the relevant tiles.
+			// This will allow to make sense of scale parameter 
+			int scale = 1;
 			for(Tile<O> tile : pendingTiles)
-				renderTile(gl, tile, grid);
+				renderTile(gl, tile, grid, scale);
 			endFrameBufferSpace( gl );
 			
 			pendingTiles = null;
 		}
 	}
 	
-	protected abstract void renderTile(GL gl, Tile<O> tile, G grid);
+	/**
+	 * Renders a single tile of terrain texture
+	 * 
+	 * @param gl
+	 * @param tile
+	 * @param grid
+	 * @param scale
+	 */
+	protected abstract void renderTile(GL gl, Tile<O> tile, G grid, int scale);
 	
 	public void setDebugOverlay(boolean debug)
 	{
 		this.debug = debug;
 	}
 	
-	private void initDebug(GL gl, G grid)
+	private boolean initDebug(GL gl, G grid)
 	{
 		float minx = grid.getMinX();
 		float maxx = grid.getMaxX();
@@ -193,8 +230,8 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 		
 	 	// tiles
 		gl.glColor4f(0,0.5f,0, 0.2f);
-		for(float x = minx; x <= maxx; x += grid.getCellSize())
-			for(float y = miny; y <= maxy; y += grid.getCellSize())
+		for(float x = minx; x < maxx; x += grid.getCellSize())
+			for(float y = miny; y < maxy; y += grid.getCellSize())
 			{
 				if(!grid.isEmptyAt(x, y))
 				{
@@ -227,6 +264,17 @@ public abstract class TileGridLook <O, G extends IGrid <Tile<O>>> implements Loo
 		}
 		
 		debugMesh.end( gl );
+		
+		return true;
 
 	}
+	
+
+	private boolean renderDebug(GL gl)
+	{
+		if(debug)
+			debugMesh.call( gl );
+		return true;
+	}
+
 }
