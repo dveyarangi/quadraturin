7936ce6c96dc183971b68f634fe290e498c4a9b8
dveyarangi <chukchac@yahoo.com>
2012-05-07 22:47:22 +0300

    moved to util repo

--- quadraturin-core/src/main/java/yarangi/graphics/quadraturin/resources/GLResource.java
+++
@@ -1,9 +0,0 @@
-package yarangi.graphics.quadraturin.resources;
-
-import javax.media.opengl.GL;
-
-
-public interface GLResource extends IResource 
-{
-	public void init(GL gl);
-}
--- quadraturin-core/src/main/java/yarangi/graphics/quadraturin/resources/IResourceManager.java
+++
@@ -1,5 +0,0 @@
-package yarangi.graphics.quadraturin.resources;
-
-public interface IResourceManager {
-
-}
